112cde96263b790fdce710d918e0cd115c0095d8
rox <roxmarzan14@gmail.com>
2024-07-11 23:46:00 +0800

    questions added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Select from './Select';
 import { motion } from 'framer-motion';
 import Questions from './Questions';
@@ -9,6 +9,11 @@ const DataFetchingComponent = () => {
     const [error, setError] = useState(true);
     const [key, setKey] = useState(0);
     const [url2, setUrl2] = useState('https://opentdb.com/api.php?amount=1');
+    const [isAnimated, setIsAnimated] = useState(false);
+    const [isCountdown, setIsCountdown] = useState(false);
+    const isCountdownRef = useRef(isCountdown);
+    const [time, setTime] = useState(5);
+    const [isNext, setIsnext] = useState(false);
 
     let api = 'https://opentdb.com/api.php?amount=1';
 
@@ -37,12 +42,16 @@ const DataFetchingComponent = () => {
                     throw new Error('Network response was not ok');
                 }
                 return response.json();
-                // console.log(response.json());
             })
             .then((data) => {
+                setTime(5);
+                setIsCountdown(false);
                 setQuestions(data.results);
                 setLoading(false);
                 console.log(data.results);
+                setKey((prevKey) => prevKey + 1);
+                setIsAnimated(true);
+                handleCountdown();
             })
             .catch((error) => {
                 setError(error);
@@ -53,6 +62,24 @@ const DataFetchingComponent = () => {
     const handleReload = () => {
         fetchQuestions(url2);
     };
+
+    const handleCountdown = () => {
+        for (let i = 0; i < 5; i++) {
+            setTimeout(
+                () => {
+                    setTime((prevCountdown) => prevCountdown - 1);
+                    if (i === 4) {
+                        isCountdownRef.current = true; // Update ref value directly
+                        setIsCountdown(true); // Trigger state update if needed for UI changes
+                        console.log(isCountdownRef.current); // Log current ref value
+                    }
+                },
+                (i + 1) * 1000,
+            );
+        }
+    };
+
+    console.log(1);
 
     return (
         <div className="container  main-container">
@@ -68,18 +95,31 @@ const DataFetchingComponent = () => {
                     >
                         QUIZLET
                     </motion.h1>
-                    <div className="box p-4">
+                    <motion.div className="box p-4" initial={{ y: 100 }} animate={isAnimated ? { y: -100, opacity: 0, display: 'none' } : { y: 0, opacity: 1 }} transition={{ duration: 0.5 }}>
                         <Select getData={getData}></Select>
-                    </div>
-                    <div className="question-box p-4">
+                    </motion.div>
+                    <motion.div
+                        className="question-box p-4"
+                        initial={{ display: 'none', opacity: 0, y: 100, transition: { duration: 0 } }}
+                        animate={!isAnimated ? { display: 'none', opacity: 0, y: 100, transition: { duration: 0 } } : { y: 0, opacity: 1, display: 'block' }}
+                        transition={{ delay: 0.5 }}
+                    >
                         {questions.map((question) => (
-                            <Questions key={key} question={question.question} />
+                            <Questions
+                                key={key}
+                                type={question.type}
+                                question={question.question}
+                                op1={question.incorrect_answers[0]}
+                                op2={question.incorrect_answers[1]}
+                                op3={question.incorrect_answers[2]}
+                                op4={question.correct_answer}
+                            />
                         ))}
 
-                        <button onClick={handleReload} className="btn btn-primary mt-3 w-100">
-                            Next
+                        <button onClick={handleReload} className="btn btn-primary w-100" disabled={isNext && isCountdown ? false : true}>
+                            {isCountdown ? 'Confirm' : time}
                         </button>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 function Questions(props) {
+    let options = [];
+    const [clickedIndex, setClickedIndex] = useState(null);
+
+    if (props.type !== 'boolean') {
+        options = [props.op1, props.op2, props.op3, props.op4];
+    } else {
+        options = [props.op1, props.op4];
+    }
+
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
+    const shuffledOptions = shuffleArray(options);
+
+    const handleAnswer = (index) => {
+        console.log(`Button ${index} clicked`);
+        setClickedIndex(index);
+    };
+
     return (
         <>
-            <p className="question" dangerouslySetInnerHTML={{ __html: props.question }}></p>
+            <motion.p
+                initial={{
+                    x: 50,
+                    opacity: 0,
+                }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="question"
+                dangerouslySetInnerHTML={{ __html: props.question }}
+            ></motion.p>
+
+            {shuffledOptions.map((option, index) => (
+                <motion.button
+                    initial={{
+                        x: 50,
+                        opacity: 0,
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        y: clickedIndex === index ? 10 : 0, // Change background color if clicked
+                    }}
+                    transition={{ duration: index + 0.5 }}
+                    key={index}
+                    className="btn btn-secondary mb-3 w-100"
+                    dangerouslySetInnerHTML={{ __html: option }}
+                    onClick={handleAnswer}
+                ></motion.button>
+            ))}
         </>
     );
 }
